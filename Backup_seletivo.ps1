0a7227d6d14d371e49cc8205db68c712ac3a90b7
Adiog0 <adriano_gdiogo@hotmail.com>
2020-04-21 20:02:25 -0300

    Backup Seletivo

---
+++ Backup_seletivo.ps1
@@ -0,0 +1,33 @@
+#Script para backup seletivo de arquivos
+
+$sourcePath = 'C:\Users\adria\Documents\bck1'
+$destinationPath = 'C:\Users\adria\Documents\bck2'
+Clear-Host
+Write-Host "Backup de arquivos seletivos"
+Write-Host "Escolha o disco de origem, 1 para C, 2para D"
+$discOrig = Read-Host
+Switch ($discOrig)
+      {
+       1 { $sourcePath = 'C:\Users\adria\Documents\bck1' }
+       2 { $sourcePath = 'D:\' }
+       }
+
+Write-Host " Escolha o disco de Destino, 1 para C, 2 para D "
+$discDestin = Read-Host
+
+Switch ($discOrig)
+      {
+       1 { $destinationPath = 'C:\Users\adria\Documents\bck2' }
+       2 { $destinationPath = 'D:\' }
+       }
+Write-Host "Informe quais tipos de arquivos deseja fazer backup: 1 Todos, 2 imagens, 3 audio, 4 videos"
+$opc = Read-Host
+
+
+Switch ($opc)
+      {
+       1 { Robocopy.exe  $sourcePath $destinationPath *.* /s /v /Z /R:3 /W:10 /XO /NP }
+       2 { Robocopy.exe  $sourcePath $destinationPath *.jpg* /s /v /Z /R:3 /W:10 /XO /NP }
+       3 { Robocopy.exe  $sourcePath $destinationPath  *.mp3* *.wma* /s /v /Z /R:3 /W:10 /XO /NP }
+       4 { Robocopy.exe  $sourcePath $destinationPath *.mp4* *.avi* /s /v /Z /R:3 /W:10 /XO /NP }
+      }
